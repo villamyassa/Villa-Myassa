3c5198ef11ea943d1c765f12031c1d05f3d451c5
villamyassa <gmenard71@gmail.com>
2026-09-15 18:14:32 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,26 @@
 const nextConfig = {
   images: { unoptimized: true }, // on désactive l’optimiseur Next
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/fr/villa-ubud-piscine-privee",
+        destination: "/fr",
+        statusCode: 301,
+      },
+      {
+        source: "/en/ubud-villa-private-pool",
+        destination: "/en",
+        statusCode: 301,
+      },
+      {
+        source: "/id/vila-ubud-kolam-renang-pribadi",
+        destination: "/id",
+        statusCode: 301,
+      },
+    ];
+  },
 };
+
 export default nextConfig;
